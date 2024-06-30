66b3d75810f47401420ed19413ad0cd35b912905
lastefanov <lacstefanov@gmail.com>
2024-06-30 18:57:28 +0300

    changing URL in tests file.

--- e2e.py
+++ e2e.py
@@ -27,6 +27,6 @@ def test_score_service(url):
     finally:
         driver.quit()
 
-url = "http://host.docker.internal:5001/"
+url = "http://172.17.0.3:5001/"
 result = test_score_service(url)
 print("Score is within range: ", result)
