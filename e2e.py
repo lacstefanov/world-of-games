bdfb26df75af1f36f1c818a53932881ceba3ff2e
lastefanov <lacstefanov@gmail.com>
2024-06-30 23:52:39 +0300

    setting back to old localhost config.

--- e2e.py
+++ e2e.py
@@ -31,6 +31,6 @@ def test_score_service(url):
     finally:
         driver.quit()
 
-url = "http://flask_container:5001/"
+url = "http://127.0.0.1:5001/"
 result = test_score_service(url)
 print("Score is within range: ", result)
