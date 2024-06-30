b6df1ff253202df62f1b42c2b6e836e94f659fbd
lastefanov <lacstefanov@gmail.com>
2024-06-30 17:30:02 +0300

    Further tests file edit.

--- e2e.py
+++ e2e.py
@@ -1,20 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+
 def test_score_service(url):
-    chrome_driver_path = "/usr/local/bin/chromedriver"  # Adjust path as per your Docker setup
     chrome_options = Options()
     chrome_options.add_argument("--headless")  # Run Chrome in headless mode (no GUI)
     chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
     chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
 
-    # Specify ChromeDriver and options
-    service = Service(chrome_driver_path)
-    driver = webdriver.Chrome(service=ChromeDriverManager().install(), options=chrome_options)
+    # Initialize ChromeDriver using webdriver_manager
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
     try:
         driver.get(url)
