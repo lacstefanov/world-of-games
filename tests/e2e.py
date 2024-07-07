bbb0c5a166bd6f5561b330bad119d65e333441db
lastefanov <lacstefanov@gmail.com>
2024-07-07 14:45:30 +0300

    adjusting jenkinsfile to work with the new test location and updated tests file in that folder.

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,4 +1,6 @@
-import time
+import os
+import logging
+import sys
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -8,29 +10,30 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
+url = os.getenv('APP_URL', 'http://world_of_games_container:5001/')
+
+
 def test_score_service(url):
     chrome_options = Options()
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    chrome_binary_path = '/opt/google/chrome/chrome'
+    chrome_binary_path = '/usr/bin/google-chrome'
     service = Service(ChromeDriverManager().install())
     chrome_options.binary_location = chrome_binary_path
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    print(driver)
-
     try:
-        time.sleep(30)
         driver.get(url)
         score_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "score")))
         score_text = score_element.text
         score = int(score_text)
         is_within_range = 1 <= score <= 1000
+
         return is_within_range
     finally:
         driver.quit()
 
-url = "http://127.0.0.1:5001/"
+
 result = test_score_service(url)
-print("Score is within range: ", result)
+
